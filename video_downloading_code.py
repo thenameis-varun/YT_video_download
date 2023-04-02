b50e1d631c4db4a90f8c869457297cdeda384287
itsSimplyVarun <119106180+thenameis-varun@users.noreply.github.com>
2023-04-02 17:46:42 +0530

    Update and rename youtube.py to video_downloading_code.py

--- video_downloading_code.py
+++ video_downloading_code.py
@@ -9,7 +9,7 @@ print(" Title : ", yt.title,"\n")
 yd = yt.streams.get_highest_resolution()
 
  
-path=r"C:\Users\91789\OneDrive\Desktop\my prog\python\YT videos"
+path=r"_PATH_OF_DIRETORY_"
 # \ in python used as a escape character so 'r' is to ignore
 
 yd.download(output_path=path)
